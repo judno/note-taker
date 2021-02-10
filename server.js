f48488f78c1ece35966cca65aee39714dec9fed5
udahjay <judahjaimeslatter@gmail.com>
2021-02-10 22:12:06 +1100

    Add static and get api route

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { json } = require("express");
 const app = express();
 
-const noteHtml = "/public/notes.html";
-const indexHtml = "/public/index.html";
+const noteHtmlPath = path.join(__dirname, "/public/notes.html");
+const indexHtmlPath = path.join(__dirname, "/public/index.html");
+const jsonPath = path.join(__dirname, "/db/db.json");
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, noteHtml));
+  res.sendFile(noteHtmlPath);
 });
 
+app.get("/api/notes", function (req, res) {
+  fs.readFile(jsonPath, "utf-8", (err, data) => {
+    res.type("application/json").send(data);
+  });
+});
+
+app.use(express.static("public"));
+
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, indexHtml));
+  res.sendFile(indexHtmlPath);
 });
 
 app.listen(3000);
