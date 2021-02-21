bef7c2399b0a4a98aac98483fb992a25ca2d190a
udahjay <judahjaimeslatter@gmail.com>
2021-02-21 15:24:26 +1100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { json } = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
